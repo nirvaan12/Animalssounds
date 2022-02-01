80a379d6ae15d3d68e9e192b62987db3e994d5ee
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-02-01 17:46:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 //https://teachablemachine.withgoogle.com/models/HlUJz5US6/
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("//https://teachablemachine.withgoogle.com/models/HlUJz5US6/model.json",modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error,results){}
